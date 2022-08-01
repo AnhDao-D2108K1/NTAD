ea7a3837d7b1f8e0176346ff4708385a9c31d48b
AnhDao-D2108K1 <108144335+AnhDao-D2108K1@users.noreply.github.com>
2022-08-01 18:03:35 +0700

    Add files via upload

---
+++ typed.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+var typed = new Typed('.element', {
+  strings: ['This is a JavaScript library','Web Developer','Web Designer','Photograper'],
+  smartBackspace: true,
+	loop: true,
+  loopCount: Infinity,
+	backSpeed: 90,
+	typeSpeed: 100,
+	// Default value
+});
